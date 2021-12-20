5331c78ac787b854ef261cbba89a7b0cb6646d20
Anand-Nadpara <86475826+Anand-Nadpara@users.noreply.github.com>
2021-12-20 14:02:35 +0530

    Add files via upload

---
+++ SPM-Group30-Project/Front-end/src/components/contact Us/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const Contact = () =>
+{
+    return (
+        <React.Fragment>
+          
+       
+
+
+        </React.Fragment>
+    )
+}
+export default Contact;
